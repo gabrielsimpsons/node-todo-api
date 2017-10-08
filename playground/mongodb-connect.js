45f9505b56c1fbf3663dbd0d8176004e165ff6eb
Constant Concept <gabrielsimpsons@outlook.com>
2017-10-08 17:17:07 -0500

    First commit

---
+++ playground/mongodb-connect.js
@@ -0,0 +1,38 @@
+const MongoClient = require('mongodb').MongoClient
+
+MongoClient.connect('mongodb://localhost:27017/todoApp-db', (error, db) => {
+  if (error) {
+    return console.log(`Unable to connect to mongodb server: ${error}`)
+  }
+  console.log('Connected to Mongodb server')
+
+  // db.collection('todo').insertOne({
+  //   text: 'Something to do',
+  //   completed: false
+  // }, (error, result) => {
+  //   if(error){
+  //     return console.log('Unable to insert todo')
+  //   }
+  //   console.log(`result-> ${JSON.stringify(result.ops, undefined, 2)}`)
+  // })
+
+  // db.collection('users').insertOne({
+  //   name: 'Gabriel',
+  //   age: 31,
+  //   location: 'Bogotá'
+  // }, (error, result) => {
+  //   if(error){
+  //     console.log(`Unable to insert user-> ${error}`)
+  //   }
+  //   console.log('User insert result->', JSON.stringify(result))
+  // })
+
+  db.collection('users').find({
+    name: 'Gabriel'
+  }).toArray().then(users => {
+    console.log(`Users found -> ${JSON.stringify(users, undefined, 2)}`)
+  }, error => {
+    console.log(`Error searching users-> ${error}`)
+  })
+  db.close()
+});
