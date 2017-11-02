0bdfce89b5e0092e34b9ffde8ce76039d6e85534
Constant Concept <gabrielsimpsons@outlook.com>
2017-11-01 20:03:37 -0500

    Environtment variable correction

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -1,6 +1,6 @@
 const MongoClient = require('mongodb').MongoClient
 
-MongoClient.connect('mongodb://localhost:27017/todoApp-db', (error, db) => {
+MongoClient.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/todoApp-db', (error, db) => {
   if (error) {
     return console.log(`Unable to connect to mongodb server: ${error}`)
   }
