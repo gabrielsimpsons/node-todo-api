d23dc43ad4d110eed28b7fdca88279735c28cdfe
Constant Concept <gabrielsimpsons@outlook.com>
2017-11-07 21:23:30 -0500

    hash password

--- server/models/user.js
+++ server/models/user.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 const jwt = require('jsonwebtoken');
 const _ = require('lodash');
+const bcrypt = require('bcryptjs')
 
 var UserSchema = new mongoose.Schema({
   email: {
@@ -70,6 +71,25 @@ UserSchema.statics.findByToken = function (token) {
     'tokens.access': 'auth'
   })
 }
+
+UserSchema.pre('save', function (next){
+  let user = this
+
+  if(user.isModified('password')){
+    bcrypt.genSalt(10, (err, salt) => {
+      if(err) {
+        console.log(`Error hashing password ${JSON.stringify(err, undefined, 2)}`)
+      }
+      bcrypt.hash(user.password, salt, (error, hash) => {
+        user.password = hash
+        next()
+      })
+    })
+  } else {
+    next()
+  }
+
+})
 
 let User = mongoose.model('User', UserSchema);
 
